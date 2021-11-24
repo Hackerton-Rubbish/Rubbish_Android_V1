3b321ce4aabc8340d3641d576bf2471fe3fcb3b5
Anbyeonghun <abh0920one@gmail.com>
2021-11-25 02:23:51 +0900

    [UPDATE] 로그인 fragment ui 추가

---
+++ app/src/main/java/com/example/rubbiish_android/ui/LoginFragment.kt
@@ -0,0 +1,25 @@
+package com.example.rubbiish_android.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.rubbiish_android.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
