cde75c1bdfabf83689fac263d34ae038f37a0aa5
Anbyeonghun <abh0920one@gmail.com>
2021-11-25 13:41:52 +0900

    [FIX] BottomNavView 아이템 이미지 수정

--- app/src/main/java/com/example/rubbiish_android/ui/MainActivity.kt
+++ app/src/main/java/com/example/rubbiish_android/ui/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.rubbiish_android.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.rubbiish_android.R
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
