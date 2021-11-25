c7c9497de0e43e7533fb54ad2e60a33fdeeb9ce7
sinbee0402 <osb3476@gmail.com>
2021-11-25 21:00:52 +0900

    [STYLE] fragment_view_post_activity.xml

--- app/src/main/java/com/example/rubbiish_android/ui/AddPostUserActivity.kt
+++ app/src/main/java/com/example/rubbiish_android/ui/AddPostUserActivity.kt
@@ -2,6 +2,7 @@ package com.example.rubbiish_android.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rubbiish_android.R
 
 class AddPostUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
