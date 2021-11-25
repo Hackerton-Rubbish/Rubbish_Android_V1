e8181cd27725598abe27bdbb9d50b9ac9f6b5089
Anbyeonghun <abh0920one@gmail.com>
2021-11-25 12:05:05 +0900

    [UPDATE] 쓰레기 시각화 서비스 UI

---
+++ app/src/main/java/com/example/rubbiish_android/ui/VisualizeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.rubbiish_android.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.rubbiish_android.R
+
+class VisualizeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_visualize, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
