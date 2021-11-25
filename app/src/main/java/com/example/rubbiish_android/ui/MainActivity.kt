5c323812fc77989e6fcc64be48eec4171b2d005d
sinbee0402 <osb3476@gmail.com>
2021-11-25 21:16:17 +0900

    [MODIFY] MainActivity.kt

--- app/src/main/java/com/example/rubbiish_android/ui/MainActivity.kt
+++ app/src/main/java/com/example/rubbiish_android/ui/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.rubbiish_android.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.rubbiish_android.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+            .add(R.id.fl_container, Fragment())
+        transaction.commit()
     }
 }
