bb2873278937c9d2dc51069943751b0777340fd1
Anbyeonghun <abh0920one@gmail.com>
2021-11-25 16:09:59 +0900

    [UPDATE] 쓰레기 배출 시각화 로직 & UI 추가

--- app/src/main/java/com/example/rubbiish_android/ui/MainActivity.kt
+++ app/src/main/java/com/example/rubbiish_android/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.rubbiish_android.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.rubbiish_android.R
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -11,5 +12,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bnv_main.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.first -> replace(VisualizeFragment())
+                else -> false
+            }
+            true
+        }
+    }
+
+    fun replace(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fl_container, fragment).commit()
     }
 }
--- app/src/main/java/com/example/rubbiish_android/ui/VisualizeFragment.kt
+++ app/src/main/java/com/example/rubbiish_android/ui/VisualizeFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.MutableLiveData
 import com.example.rubbiish_android.R
+import kotlinx.android.synthetic.main.fragment_visualize.*
 
 class VisualizeFragment : Fragment() {
+
+    private var result = 0
+    private val literLiveData : MutableLiveData<Int> = MutableLiveData()
+    private var liter = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +26,40 @@ class VisualizeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        literLiveData.postValue(0)
+        visual_minus_fab.setOnClickListener{
+            minus()
+        }
+        visual_plus_fab.setOnClickListener{
+            plus()
+        }
+        literLiveData.observe(viewLifecycleOwner, {
+            visual_liter_cnt_tv.text = literLiveData.value!!.toString()
+        })
+        visual_away_btn.setOnClickListener{
+            if(result < literLiveData.value!! * -1)
+                result = 0
+            else
+                result += literLiveData.value!!
 
+            literLiveData.postValue(0)
+            visual_liter_tv.text = "${result}L"
+            if(result * 10 < 600)
+                wave_lottie.scrollY = (result * 10)
+            else
+                wave_lottie.scrollY = 600
+        }
+        visual_one_liter.setOnClickListener{
+            liter = 1
+        }
+        visual_five_liter.setOnClickListener{
+            liter = 5
+        }
+        visual_ten_liter.setOnClickListener{
+            liter = 10
+        }
     }
+    private fun minus() = literLiveData.postValue(literLiveData.value!! - liter)
+
+    private fun plus() = literLiveData.postValue(literLiveData.value!! + liter)
 }
